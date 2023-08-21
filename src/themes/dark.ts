a0a586ecdb61d0e01484877878c87937f61fa517
Eduardo Vieira <carlosevhs@gmail.com>
2023-08-21 15:08:00 -0300

    finalizando projeto

---
+++ src/themes/dark.ts
@@ -0,0 +1,17 @@
+const temaDark = {
+  corPrinciapal: '#fff',
+  corSecundaria: '#eee',
+  corDeFundo: '#282a35',
+  corDeFundoBotao: '#fff',
+  corDeBorda: '#c1c1c1'
+}
+
+export default temaDark
+
+export type Theme = {
+  corPrinciapal: string
+  corSecundaria: string
+  corDeFundo: string
+  corDeFundoBotao: string
+  corDeBorda: string
+}
---
+++ src/themes/light.ts
@@ -0,0 +1,9 @@
+const temaLight = {
+  corPrinciapal: '#282a35',
+  corSecundaria: '#949494',
+  corDeFundo: '#fff',
+  corDeFundoBotao: '#4476bf',
+  corDeBorda: '#c1c1c1'
+}
+
+export default temaLight
