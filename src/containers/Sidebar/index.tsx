3248c687efb0871032a13ceae54dc804129329f3
Eduardo Vieira <carlosevhs@gmail.com>
2023-08-21 10:11:06 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../Components/Avatar'
+import Paragrafo from '../../Components/Paragrafo'
 import Titulo from '../../Components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/carlosevhs.png" />
-    <Titulo fontSize={20}>Carlos Vieira</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Carlos Vieira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        carlosevhs
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
